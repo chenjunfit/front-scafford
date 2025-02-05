344136537e98c4bea7d0291b25cfd62b72c0c864
chenjunfit@gmail.com <chenjunfit@gmail.com>
2025-02-05 23:19:01 +0800

    根据校验结果设置登录状态

--- src/view/login.vue
+++ src/view/login.vue
@@ -2,9 +2,9 @@
     <el-card style="width: 480px">
         <h2>后台管理系统</h2>
         <el-form
-            ref="ruleFormRef"
+            ref="loginRef"
             style="max-width: 600px"
-            :model="ruleForm"
+            :model="loginInfo"
             status-icon
             :rules="rules"
             label-width="auto"
@@ -12,13 +12,13 @@
 
         >
             <el-form-item  prop="username">
-                <el-input :prefix-icon="User" v-model.number="loginInfo.username" />
+                <el-input :prefix-icon="User" clearable placeholder="请输入用户名" v-model="loginInfo.username" />
             </el-form-item>
-            <el-form-item prop="pass">
-                <el-input :prefix-icon="Lock" v-model="loginInfo.password" type="password" autocomplete="off" />
+            <el-form-item prop="password">
+                <el-input :prefix-icon="Lock" placeholder="请输入密码" show-password v-model="loginInfo.password" type="password" autocomplete="off" />
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="submitForm(ruleFormRef)">
+                <el-button :disabled="loginDisabled" type="primary" @click="submitForm(ruleFormRef)">
                     登录
                 </el-button>
             </el-form-item>
@@ -26,11 +26,29 @@
     </el-card>
 </template>
 <script setup>
-import {reactive} from "vue";
+import {reactive, ref,watch} from "vue";
 import { User,Lock} from '@element-plus/icons-vue'
+const loginRef = ref()
+
 const loginInfo =reactive({
     username: "",
     password: "",
+})
+const rules=reactive({
+    username:[
+        { required: true, message: '请输入用户名', trigger: 'blur' },
+    ],
+    password:[
+        { required: true, message: '请输入密码', trigger: 'blur' },
+    ],
+})
+let loginDisabled=ref(true)
+watch([()=>loginInfo.username,()=>loginInfo.password],()=>{
+    loginRef.value.validate((valid)=>{
+        if(valid){
+            loginDisabled.value=false
+        }
+    })
 })
 </script>
 
